a1ca6290658dfac08c2bf0cdfa8f49dc2f10d0bf
AkasakaID <fawwazthoerif@gmail.com>
2022-07-24 20:29:28 +0700

    :update add function parse_from_file

--- headers.py
+++ headers.py
@@ -17,3 +17,16 @@ class mitmproxyHeaders:
 			else:
 				headers[i.split(': ')[0]] = i.split(': ')[1]
 		return headers
+
+	def parse_from_file(self,file):
+		headers = {}
+		cookie = {}
+		for i in open(file,'r').read().splitlines():
+			if i.split(': ')[0].lower() == 'user-agent':
+				headers[i.split(': ')[0]] = i.split(': ')[1]
+			elif i.split(': ')[0].lower() == 'cookie':
+				cookie[i.split(': ')[1].split('=')[0]] = i.split(': ')[1].split('=')[1]
+				headers[i.split(': ')[0]] = cookie
+			else:
+				headers[i.split(': ')[0]] = i.split(': ')[1]
+		return headers
